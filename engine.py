41ecf437cc2b67b5a2a4d4fa9079b93d8a6c6168
VikingSchism <vikingschism@gmail.com>
2019-06-25 09:55:51 +0100

    Implementing entity class into engine

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 import tcod as libtcod
+
+from entity import Entity
 from input_handlers import handle_keys
 
 
@@ -6,8 +8,9 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
+    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -22,11 +25,14 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
         libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_put_char(0, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, player.x, player.y, ' ', libtcod.BKGND_NONE)
 
         action = handle_keys(key)
 
@@ -36,8 +42,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
